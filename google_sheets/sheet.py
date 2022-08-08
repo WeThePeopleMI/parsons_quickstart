0c9fa45f0e336b6ba5c26556090236d0dbeaa4de
Kate_wtpmi <97978129+kate-wtpmi@users.noreply.github.com>
2022-08-08 10:09:02 -0400

    Update sheet.py
    parsons get sheet and append row example

---
+++ google_sheets/sheet.py
@@ -0,0 +1,44 @@
+from parsons import Table, GoogleSheets
+import json
+from datetime import datetime, timedelta
+
+
+with open('service_account_parsons.json') as creds:
+  gs = GoogleSheets(google_keyfile_dict=json.load(creds))
+
+# sheet here: https://docs.google.com/spreadsheets/d/1RFCaphy9moYa5qjXIAGb57M0U3fBNBQqutQDtcy7h9s/edit#gid=0
+# confirm your service account email, under "client_email" is shared to it.
+ss_id = "1RFCaphy9moYa5qjXIAGb57M0U3fBNBQqutQDtcy7h9s"
+
+ss = gs.get_worksheet(ss_id)
+
+
+def main():
+  print(ss)
+
+  ss_dict = ss.to_dicts()
+
+  print(ss_dict)
+
+  #to overwrite the sheet after an anlysis
+  # gs.overwrite_sheet(ss_id, Table(ss_dict))
+
+  #example rows to append, have to be a parsons table
+  example_table = Table(
+        [
+          {
+            "header1":"new row!", 
+            "header2": str(datetime.now())
+          }
+        ]
+      )
+  #append
+  gs.append_to_sheet(
+      spreadsheet_id=ss_id, 
+      table=example_table,
+      worksheet=0, 
+    )
+
+
+if __name__ == "__main__":
+    main()
